dacd24fba7c0e031b32c47186c0baae826152657
Stanly Shiyanovskiy <st-small@rambler.ru>
2016-05-05 11:13:52 +0300

    Initial Commit

---
+++ 41_CoreData_DZ_/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  41_CoreData_DZ_
+//
+//  Created by Stas on 05.05.16.
+//  Copyright © 2016 Stas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 41_CoreData_DZ_/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  41_CoreData_DZ_
+//
+//  Created by Stas on 05.05.16.
+//  Copyright © 2016 Stas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
