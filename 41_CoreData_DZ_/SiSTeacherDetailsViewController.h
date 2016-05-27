6f9ab9f1a09e04fca9022e0768ad7b489fb9c070
Stanly Shiyanovskiy <st-small@rambler.ru>
2016-05-27 12:44:31 +0300

    Добавил SiSTeacherDetailsViewController

---
+++ 41_CoreData_DZ_/SiSTeacherDetailsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SiSTeacherDetailsViewController.h
+//  41_CoreData_DZ_
+//
+//  Created by Stanly Shiyanovskiy on 26.05.16.
+//  Copyright © 2016 Stas. All rights reserved.
+//
+
+#import "SiSDetailsTableViewController.h"
+
+@interface SiSTeacherDetailsViewController : SiSDetailsTableViewController
+
+@end
---
+++ 41_CoreData_DZ_/SiSTeacherDetailsViewController.m
@@ -0,0 +1,37 @@
+//
+//  SiSTeacherDetailsViewController.m
+//  41_CoreData_DZ_
+//
+//  Created by Stanly Shiyanovskiy on 26.05.16.
+//  Copyright © 2016 Stas. All rights reserved.
+//
+
+#import "SiSTeacherDetailsViewController.h"
+
+@interface SiSTeacherDetailsViewController ()
+
+@end
+
+@implementation SiSTeacherDetailsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
